c26c63833b381860750cc695fc05ea1e84241d7d
victor.krohin <victor.krohin@progforce.com>
2020-06-01 15:04:00 +0300

    first

--- config/db.php
+++ config/db.php
@@ -2,9 +2,6 @@
 
 return [
     'class' => 'yii\db\Connection',
-    // 'dsn' => 'mysql:host=mysql102.1gb.ru;dbname=dveri-vsem',
-    // 'username' => 'gb_dveri_vsem',
-    // 'password' => '33aa43c6cnmz',
     'dsn' => 'mysql:host=localhost;dbname=doors',
     'username' => 'root',
     'password' => '',
